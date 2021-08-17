f34923ad3ffe82f0f6b73d140876db1ad081ad64
Carl Dea <cdea@azul.com>
2021-08-16 20:27:31 -0400

    Code to iterate an array of structs.

--- part02/structs.c
+++ part02/structs.c
@@ -3,11 +3,20 @@
 struct Point {
   int x;
   int y;
-};
+} points[5];
 
 int main () {
    struct Point pt;
    pt.x = 100;
    pt.y = 50;
    printf("Point pt = (%d, %d) \n",  pt.x, pt.y);
+
+   for (int i=0; i<5; i++) {
+     points[i].x = 100 + i;
+     points[i].y = 200 + i;
+   }
+
+   for (int i=0; i<5; i++) {
+     printf("Point pt = (%3d, %3d) \n",  points[i].x, points[i].y);
+   }
 }
