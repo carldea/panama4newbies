da7df66e433d3083b5c549ea016205ac8e502382
Carl Dea <carldea@gmail.com>
2021-09-05 16:06:49 -0400

    Added a project for Panama to talk to Swift

---
+++ macos-hello-swift/sayhello.sh
@@ -0,0 +1,19 @@
+# clean up
+rm -rf classes
+rm *.dylib
+
+# Create a swift library
+swiftc sayhello.swift -emit-library -o libsayhelloswift.dylib
+
+# Create a c/c++ library
+gcc -dynamiclib -current_version 1.0 ./libsayhelloswift.dylib -o libsayhellocpp.dylib sayhello.cpp
+
+
+# jextract header to generate sources.
+jextract --source -d classes -t sayhello -l sayhellocpp -I /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include sayhello.h
+
+# jextract header to generate classes.
+jextract -d classes -t sayhello -l sayhellocpp -I /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include sayhello.h
+
+# Run java class
+java -cp .:classes --enable-native-access=ALL-UNNAMED --add-modules jdk.incubator.foreign SayHello.Java
